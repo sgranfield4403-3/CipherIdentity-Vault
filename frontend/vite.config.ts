1a8ceb105c79646830ab72f85a9fb1afe09a1f04
sgranfield4403-3 <laborer.streets08@icloud.com>
2025-11-29 15:45:00 +0800

    chore: configure Vite path aliases and build optimization

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,8 +9,27 @@ export default defineConfig({
   define: {
     'process.env': {}
   },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+      '@config': path.resolve(__dirname, './src/config'),
+    }
+  },
   server: {
     port: 5173,
     open: true
+  },
+  build: {
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          antd: ['antd'],
+        }
+      }
+    }
   }
 })
